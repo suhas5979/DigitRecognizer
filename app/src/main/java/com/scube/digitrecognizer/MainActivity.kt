318aeaafbd5ea0df5c2b5bd10094f1e728957ecf
suhas5979 <suhassuryavanshi5979@gmail.com>
2021-01-24 14:00:26 +0530

    screenshots added

--- app/src/main/java/com/scube/digitrecognizer/MainActivity.kt
+++ app/src/main/java/com/scube/digitrecognizer/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.scube.digitrecognizer
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.MotionEvent
@@ -11,9 +12,11 @@ import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     private var mClassifier: Classifier? = null
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setResultInvisible()
         try {
             mClassifier = Classifier(this)
         } catch (e: IOException) {
@@ -24,15 +27,28 @@ class MainActivity : AppCompatActivity() {
             val bitmap =
                 fpv_paint.exportToBitmap(Classifier.IMG_WIDTH, Classifier.IMG_HEIGHT)
             val res = mClassifier!!.classify(bitmap)
-            probability.text = "Probability: ${res.probability}"
-            prediction.text = "Prediction: ${res.number}"
-            timecost.text = "TimeCost:${ res.timeCost}"
+            setResultVisible()
+            probability.text = "${resources.getString(R.string.probability)}: ${res.probability *100}%"
+            prediction.text = "${resources.getString(R.string.prediction)}: ${res.number}"
+            timecost.text = "${resources.getString(R.string.timeCost)}:${ res.timeCost}"
         }
         btn_clear.setOnClickListener {
+
             fpv_paint.clear()
             prediction.text = ""
             probability.text = ""
             timecost.text = ""
+            setResultInvisible()
         }
+    }
+    private fun setResultVisible(){
+        prediction.visibility = View.VISIBLE
+        probability.visibility = View.VISIBLE
+        timecost.visibility = View.VISIBLE
+    }
+    private fun setResultInvisible(){
+        prediction.visibility = View.INVISIBLE
+        probability.visibility = View.INVISIBLE
+        timecost.visibility = View.INVISIBLE
     }
 }
